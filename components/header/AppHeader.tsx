e6ace51f0b27862deeb5c21c3fb9a0e7d777ae4b
unknown <raffy.amoguis.projects@gmail.com>
2023-01-17 10:41:36 +0800

    fix layout

--- components/header/AppHeader.tsx
+++ components/header/AppHeader.tsx
@@ -1,51 +1,18 @@
 import React from 'react';
-import { Image, Group, Anchor, Input, ActionIcon, Text } from '@mantine/core';
-import {
-  IconSearch,
-  IconBell,
-  IconMessage2,
-  IconMoonStars,
-} from '@tabler/icons';
+import { Group } from '@mantine/core';
+
+import RightSection from './RightSection';
 import logo from '../../assets/images/logo-no-background.png';
 import appHeaderstyle from './appheader.module.css';
-import { useDeviceSize, isDeviceMD } from '../../hooks/useDeviceSize';
-
-const headerIcons = [
-  { icon: IconBell },
-  { icon: IconMessage2 },
-  { icon: IconMoonStars },
-];
+import SearchBar from './SearchBar';
+import LeftSection from './LeftSection';
 
 const AppHeader: React.FC = () => {
-  const [width] = useDeviceSize();
   return (
-    <Group position='apart' style={{ height: '100%', alignItems: 'center' }}>
-      <Image
-        src={logo.src}
-        alt='Game account marketplace logo'
-        width={125}
-        height={25}
-        fit='contain'
-      />
-
-      <Input
-        className={appHeaderstyle.searchInput}
-        icon={<IconSearch size={20} />}
-        placeholder='Search..'
-        size={isDeviceMD(width) ? 'xs' : 'md'}
-        radius='xl'
-      />
-
-      <Group>
-        <Text>Account Type</Text>
-        {headerIcons.map((header: any, idx) => (
-          <Anchor key={idx}>
-            <ActionIcon>
-              {<header.icon size={isDeviceMD(width) ? 18 : null} />}
-            </ActionIcon>
-          </Anchor>
-        ))}
-      </Group>
+    <Group position='apart' sx={{ height: '100%' }} px={20}>
+      <LeftSection />
+      <SearchBar />
+      <RightSection />
     </Group>
   );
 };
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,14 +1,5 @@
-import React, { useState } from 'react';
-import {
-  AppShell,
-  Header,
-  Text,
-  MediaQuery,
-  Burger,
-  useMantineTheme,
-} from '@mantine/core';
-import AppNav from '../navbar/AppNav';
-import AppSideNav from '../sidenav/AppSideNav';
+import React from 'react';
+import { AppShell, Header, Container } from '@mantine/core';
 import AppHeader from '../header/AppHeader';
 
 type Props = {
@@ -16,47 +7,25 @@ type Props = {
 };
 
 const Layout: React.FC<Props> = ({ children }) => {
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
-
   return (
-    <>
-      <AppShell
-        styles={{
-          main: {
-            background:
-              theme.colorScheme === 'dark'
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-          },
-        }}
-        navbarOffsetBreakpoint='sm'
-        asideOffsetBreakpoint='sm'
-        navbar={<AppNav opened={opened} />}
-        aside={<AppSideNav />}
-        header={
-          <Header height={{ base: 50, md: 70, sm: 50 }} p='md'>
-            {/* <div
-              style={{ display: 'flex', alignItems: 'center', height: '100%' }}
-            > */}
-            <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size='sm'
-                color={theme.colors.gray[6]}
-                mr='xl'
-              />
-            </MediaQuery>
-
-            <AppHeader />
-            {/* </div> */}
-          </Header>
-        }
-      >
-        {children}
-      </AppShell>
-    </>
+    <AppShell
+      padding='md'
+      header={
+        <Header height={60} p='xs'>
+          <AppHeader />
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      <Container>{children}</Container>
+    </AppShell>
   );
 };
 
