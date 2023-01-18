a6fb088771f4b9eec5653dc438b0887082032cbe
unknown <raffy.amoguis.projects@gmail.com>
2023-01-18 12:24:02 +0800

    layout responsive

--- components/header/AppHeader.tsx
+++ components/header/AppHeader.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Group } from '@mantine/core';
 
 import RightSection from './RightSection';
-import logo from '../../assets/images/logo-no-background.png';
+
 import appHeaderstyle from './appheader.module.css';
 import SearchBar from './SearchBar';
 import LeftSection from './LeftSection';
+import { useDeviceSize, isDeviceSM } from '../../hooks/useDeviceSize';
 
 const AppHeader: React.FC = () => {
+  const [width] = useDeviceSize();
   return (
-    <Group position='apart' sx={{ height: '100%' }} px={20}>
+    <Group
+      position='apart'
+      sx={{ height: '100%' }}
+      px={isDeviceSM(width) ? 0 : 20}
+    >
       <LeftSection />
       <SearchBar />
       <RightSection />
--- components/header/LeftSection.tsx
+++ components/header/LeftSection.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import Link from 'next/link';
-import { Text, Group, Anchor, Button } from '@mantine/core';
+import { Text, Group, Anchor, Avatar, MediaQuery } from '@mantine/core';
+import logo from '../../assets/images/logo-1.png';
 
 const LeftSection = () => {
   return (
     <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-      <Text color='orange' fw={700}>
-        Game Account Marketplace
-      </Text>
-      <Group spacing='xs' sx={{ marginLeft: '2em' }}>
-        <Anchor component={Link} href='#' underline={false} color='black'>
-          Home
-        </Anchor>
-        <Anchor component={Link} href='#' underline={false} color='gray'>
-          Merchant
-        </Anchor>
-      </Group>
+      <MediaQuery largerThan='md' styles={{ display: 'none' }}>
+        <Avatar src={logo.src} alt='Game market place logo.' />
+      </MediaQuery>
+      <MediaQuery smallerThan='md' styles={{ display: 'none' }}>
+        <Text color='orange' fw={700}>
+          Game Account Marketplace
+        </Text>
+      </MediaQuery>
+
+      <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+        <Group spacing='xs' sx={{ marginLeft: '2em' }}>
+          <Anchor component={Link} href='#' underline={false} color='black'>
+            Home
+          </Anchor>
+          <Anchor component={Link} href='#' underline={false} color='gray'>
+            Merchant
+          </Anchor>
+        </Group>
+      </MediaQuery>
     </div>
   );
 };
